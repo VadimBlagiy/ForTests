f1ab170d67c06b930abed4f14452652de3d5e945
Vadim Blagiy <vadblagiy@gmail.com>
2021-06-23 10:10:49 +0300

    Revert "Test6"
    This reverts commit 7124a9e28989c785855e9696e97fd7682fbb3678.

--- src/main/java/Test5.java
+++ src/main/java/Test5.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
-public class TTTest6 {
+public class Test5 {
     public static void main(String[] args) {
 
 //            int sum = 0;
-//            int znak = 1;ffgdfg dfgs
-//sfgs
+//            int znak = 1;
+//
 //            for(int i = 1; i <= 6; i++){
 //                sum += znak*i;
 //                znak *= -1;
